67806eec1d88d6f1b99595efa65e670f029cd7cc
ahmed-zubair12 <a.zubair@arbisoft.com>
2021-01-25 16:22:59 +0500

    MCKIN-29100 Disabling button on submition to avoid multiple requests

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ BLOCKS_CHILDREN = [
 
 setup(
     name='xblock-adventure',
-    version='0.4.2',
+    version='0.4.3',
     description='XBlock - Adventure',
     packages=['adventure'],
     install_requires=[
